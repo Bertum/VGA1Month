9b896e411e125a9ef1f9826a21aa0f43d430e14f
Bertum <albertodelcru@gmail.com>
2018-09-30 23:31:29 +0200

    MainClass implemented

--- js/Classes/Powerup.js
+++ js/Classes/Powerup.js
@@ -1,8 +1,7 @@
+Powerup.prototype = new MainClass();
 //Declaracion objeto Powerup
 function Powerup(src, powX, powY, efecto, tiempo) {
-    this.sprite = src;
-    this.powX = powX;
-    this.powY = powY;
+    MainClass.call(this, src, powX, powY);
     this.efecto = efecto;
     this.altura = this.sprite.height;
     this.anchura = this.sprite.width;
